4c73b8f403502f2c4a057fc334c68b7d5e1db300
cronopio <michelleperalbo@gmail.com>
2025-05-02 14:11:00 +0000

    delete not used link

--- apps/www/src/utils/utils.ts
+++ apps/www/src/utils/utils.ts
@@ -1,4 +1,4 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const API_BASE = process.env.NEXT_PUBLIC_API_URL;
 
 export function urlHandler(path: string): string {
     return `${API_BASE}${path}`;
